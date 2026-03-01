e0ba8b4ea3dc5bd58418c493f56c4926cc0c2a31
Mikiyas-hs <mikiyashadis160@gmail.com>
2026-03-01 14:02:14 +0100

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -20,6 +20,11 @@ export const metadata = {
   description: "Webshop where you can buy your UV-protected gloves",
 };
 
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+};
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
